194d7d96888e3e98a9a3add2515dbd2954443d0c
juanfranfdezGit <jfranromfdez@gmail.com>
2025-12-16 19:22:16 +0100

    feat: maps

--- src/campaigns/ElLatidodelaMáquina-Diosa/map.js
+++ src/campaigns/ElLatidodelaMáquina-Diosa/map.js
@@ -1,17 +1,41 @@
 import "./map.css";
+import { useState } from "react";
 
 export default function LatidoMap() {
+  const [actualState, setActualState] = useState(1);
+
+  const maps = {
+    1: "/assets/maps/latido01.png",
+    2: "/assets/maps/latido02.png",
+    3: "/assets/maps/latido03.png",
+  };
+
   return (
     <section className="mapSection flex column">
-        <nav className="actsLinks">
-            <ul className="flex">
-                <li>Acto I</li>
-                <li>Acto II</li>
-                <li>Acto III</li>
-            </ul>
-        </nav>
+      <nav className="actsLinks">
+        <ul className="flex">
+          <li
+            className={actualState === 1 ? "active" : ""}
+            onClick={() => setActualState(1)}
+          >
+            Acto I
+          </li>
+          <li
+            className={actualState === 2 ? "active" : ""}
+            onClick={() => setActualState(2)}
+          >
+            Acto II
+          </li>
+          <li
+            className={actualState === 3 ? "active" : ""}
+            onClick={() => setActualState(3)}
+          >
+            Acto III
+          </li>
+        </ul>
+      </nav>
       <img
-        src="/assets/maps/latido01.png"
+        src={maps[actualState]}
         alt="Mapa del Mundo Fronterizo Helion-Σ"
         className="mapImage"
       />
