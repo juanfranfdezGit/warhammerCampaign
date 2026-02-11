fa48f1a1034cd502f9178cdd9371b6f1b61a6be1
Juan Francisco Romero Fernández <jfranromfdez@gmail.com>
2026-02-11 11:39:34 +0100

    feat: lapCounter and finishLine

--- src/campaigns/ElLatidodelaMáquina-Diosa/armies.js
+++ src/campaigns/ElLatidodelaMáquina-Diosa/armies.js
@@ -1,4 +1,5 @@
-import armiesData from "../../datas/armies/mechanicus.json";
+import mecha from "../../datas/armies/mechanicus.json";
+import imperial from "../../datas/armies/puños.json"
 import "./armies.css";
 
 export default function LatidoArmies() {
@@ -6,8 +7,8 @@ export default function LatidoArmies() {
     <section className="armiesSection">
       <h2>Ejércitos</h2>
 
-      <h3>{armiesData[0]?.faction}</h3>
-      {armiesData.map((army) => (
+      <h3>{mecha[0]?.faction}</h3>
+      {mecha.map((army) => (
         <table key={army.id} className="datasheetTable">
           <thead>
             {/* Nombre + Facción */}
@@ -30,10 +31,10 @@ export default function LatidoArmies() {
 
           <tbody>
             <tr className="statsRow">
-              <td>{army.movement}</td>
-              <td>{army.resist}</td>
-              <td>{army.salvation}</td>
-              <td>{army.wound}</td>
+              <td>{army.movimiento}</td>
+              <td>{army.resistencia}</td>
+              <td>{army.salvacion}</td>
+              <td>{army.herida}</td>
               <td>{army.leadership}</td>
               <td>{army.control}</td>
             </tr>
@@ -72,10 +73,91 @@ export default function LatidoArmies() {
                           <h4>{weapon.name}:</h4>
                           <p>Rango: {weapon.range}</p>
                           <p>Utilizando: {weapon.number}</p>
-                          <p>Fuerza: {weapon.strength}</p>
-                          <p>Penetración: {weapon.ap}</p>
-                          <p>Daño: {weapon.damage}</p>
-                          <p>Disparos: {weapon.shots}</p>
+                          <p>Fuerza: {weapon.fuerza}</p>
+                          <p>Penetración: {weapon.penetración}</p>
+                          <p>Daño: {weapon.daño}</p>
+                          <p>Disparos: {weapon.ataques}</p>
+                          <p>{weapon.abilities}</p>
+                        </li>
+                      ))}
+                    </ul>
+                  </td>
+                </tr>
+              </>
+            )}
+          </tbody>
+        </table>
+      ))}
+      <h3>{imperial[0]?.faction}</h3>
+      {imperial.map((army) => (
+        <table key={army.id} className="datasheetTable">
+          <thead>
+            {/* Nombre + Facción */}
+            <tr className="unitHeader">
+              <th colSpan="6">
+                <p>{army.name}</p>
+              </th>
+            </tr>
+
+            {/* Stats */}
+            <tr className="statsHeader">
+              <th>M</th>
+              <th>T</th>
+              <th>Sv</th>
+              <th>W</th>
+              <th>Ld</th>
+              <th>OC</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr className="statsRow">
+              <td>{army.movimiento}</td>
+              <td>{army.resistencia}</td>
+              <td>{army.salvacion}</td>
+              <td>{army.herida}</td>
+              <td>{army.leadership}</td>
+              <td>{army.control}</td>
+            </tr>
+
+            {/* Habilidades */}
+            {army.abilities?.length > 0 && (
+              <>
+                <tr className="sectionTitle">
+                  <td colSpan="6">Habilidades</td>
+                </tr>
+                <tr>
+                  <td colSpan="6">
+                    <ul className="abilitiesList">
+                      {army.abilities.map((ability) => (
+                        <li key={ability.id}>
+                          <strong>{ability.name}:</strong> {ability.description}
+                        </li>
+                      ))}
+                    </ul>
+                  </td>
+                </tr>
+              </>
+            )}
+
+            {/* Armas */}
+            {army.weapons?.length > 0 && (
+              <>
+                <tr className="sectionTitle">
+                  <td colSpan="6">Armas</td>
+                </tr>
+                <tr>
+                  <td colSpan="6">
+                    <ul className="weaponsList">
+                      {army.weapons.map((weapon, index) => (
+                        <li key={index} className="weaponItem flex column">
+                          <h4>{weapon.name}:</h4>
+                          <p>Rango: {weapon.range}</p>
+                          <p>Utilizando: {weapon.number}</p>
+                          <p>Fuerza: {weapon.fuerza}</p>
+                          <p>Penetración: {weapon.penetración}</p>
+                          <p>Daño: {weapon.daño}</p>
+                          <p>Disparos: {weapon.ataques}</p>
                           <p>{weapon.abilities}</p>
                         </li>
                       ))}
