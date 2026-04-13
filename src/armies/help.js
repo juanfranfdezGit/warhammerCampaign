84427330fbd76757f715228f2e1c9ec587a203f3
Juan Francisco Romero Fernández <jfranromfdez@gmail.com>
2026-04-13 18:59:07 +0200

    feat: reglas nuevas

--- src/armies/help.js
+++ src/armies/help.js
@@ -1,4 +1,5 @@
 import Habilidades from "../datas/help/habilities.json";
+import Fases from "../datas/help/gamePhases.json";
 import "./help.css";
 import { Link } from "react-router-dom";
 
@@ -12,6 +13,18 @@ export default function Help() {
       </Link>
 
       <h1>Ayudas</h1>
+
+       <ul>
+        <h2>Fases del Juego</h2>
+        {Fases.map((fase) => (
+          <li key={fase.id}>
+            <h3>{fase.name}</h3>
+            <p>{fase.description}</p>
+            <p className="ejs">{fase.description2}</p>
+          </li>
+        ))}
+      </ul>
+
       <ul>
         <h2>Habilidades</h2>
         {Habilidades.map((habilidad) => (
