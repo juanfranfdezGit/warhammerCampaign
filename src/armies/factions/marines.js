c247747a24726471bcfed67b56d63fffa3efd917
Juan Francisco Romero Fernández <jfranromfdez@gmail.com>
2026-04-14 17:08:56 +0200

    feat: imperial added detachment

--- src/armies/factions/marines.js
+++ src/armies/factions/marines.js
@@ -1,19 +1,152 @@
 import imperial from "../../datas/armies/puños.json";
+import destacamentos from "../../datas/armies/puñosFaction.json";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "../armies.css";
 
 export default function Marines() {
+  const doctrinasData = destacamentos.find((item) => item.doctrinas);
+  const destacamentoData = destacamentos.find((item) => item.destacamentos);
+  const estratagemaData = destacamentos.find((item) => item.estratagemas);
+  const [listActive, setListActive] = useState(null);
+
+  const toggleList = () => {
+    setListActive(!listActive);
+  };
+
   return (
     <section className="armiesSection">
-      <Link to="/armies">
-        <div className="backArmieBtn">
-          <img src="/assets/icos/back.png" alt="back btn"></img>
+      <div className="navTopBar">
+        <Link to="/armies">
+          <div className="backArmieBtn armiePage">
+            <img src="/assets/icos/back.png" alt="back btn"></img>
+          </div>
+        </Link>
+
+        <div className="unitLinks">
+          <img
+            src="/assets/icos/imperialFist.png"
+            alt="imperial fist banner"
+            className="factionBanner"
+            onClick={toggleList}
+          ></img>
+
+          <div className={`unitsList ${listActive ? "active" : ""}`}>
+            <ul>
+              {imperial.map((unit) => (
+                <li key={unit.id}>
+                  <a href={`#${unit.id}`} className="unitLink">
+                    {unit.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </Link>
+
+        <Link to="/help">
+          <div className="helpArmieBtn">
+            <img src="/assets/icos/help.png" alt="help btn"></img>
+          </div>
+        </Link>
+      </div>
 
       <div className="armiesHeader">
         <h1>{imperial[0].faction}</h1>
       </div>
+
+      {doctrinasData && (
+        <div className="doctrinasSection">
+          <h2>Reglas de ejército</h2>
+
+          {doctrinasData.doctrinas.map((doctrina) => (
+            <div key={doctrina.id}>
+              <h3>{doctrina.name}</h3>
+              <p>{doctrina.description1}</p>
+              <p>{doctrina.description2}</p>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {destacamentoData && (
+        <div className="doctrinasSection destacamentosSection">
+          <h2>Destacamentos</h2>
+
+          {destacamentoData.destacamentos.map((destacamento) => (
+            <div key={destacamento.id}>
+              <h3>{destacamento.name}</h3>
+              <p>{destacamento.description}</p>
+              <h4>{destacamento.doc1title}</h4>
+              <p>{destacamento.doc1}</p>
+              <h4>{destacamento.doc2title}</h4>
+              <p>{destacamento.doc2}</p>
+              <h4>{destacamento.doc3title}</h4>
+              <p>{destacamento.doc3}</p>
+              <div className="mejoraSection">
+                <h3>Mejoras</h3>
+                <h4
+                  dangerouslySetInnerHTML={{
+                    __html: destacamento.mejoratitle1,
+                  }}
+                />
+                <p>{destacamento.mejoradescription1}</p>
+                <h4
+                  dangerouslySetInnerHTML={{
+                    __html: destacamento.mejoratitle2,
+                  }}
+                />
+                <p>{destacamento.mejoradescription2}</p>
+                <h4
+                  dangerouslySetInnerHTML={{
+                    __html: destacamento.mejoratitle3,
+                  }}
+                />
+                <p>{destacamento.mejoradescription3}</p>
+                <h4
+                  dangerouslySetInnerHTML={{
+                    __html: destacamento.mejoratitle4,
+                  }}
+                />
+                <p>{destacamento.mejoradescription4}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {estratagemaData && (
+        <div className="doctrinasSection estratagemasSection">
+          <h2>Estratagemas</h2>
+
+          {estratagemaData.estratagemas.map((estratagema) => (
+            <div
+              key={estratagema.id}
+              className={`estratagemaGrid estratagema-${estratagema.id}`}
+            >
+              <div className="estratagemaSide">
+                <div className="estratagemaImgContainer">
+                  <img
+                    src={estratagema.ico}
+                    alt={estratagema.name}
+                    className="estratagemaImg"
+                  />
+                </div>
+                <div className="estratagemaValueContainer">
+                  <p className="estratagemaValue">{estratagema.value}</p>
+                </div>
+              </div>
+              <div className="estragemaInfo">
+                <h3>{estratagema.name}</h3>
+                <p>{estratagema.description1}</p>
+                <p>{estratagema.when}</p>
+                <p>{estratagema.target}</p>
+                <p>{estratagema.effect}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
 
       {imperial.map((army) => (
         <table key={army.id} className="datasheetTable">
